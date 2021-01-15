6315da3ac38ab816d45ec0b4a20761b27701d00d
Mike Meyer <mike@meyermike.com>
2021-01-15 11:13:10 -0500

    Generic ProgressDialog

---
+++ BloodstarClockticaWpf/ProgressDialog.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace BloodstarClockticaWpf
+{
+    /// <summary>
+    /// Interaction logic for ProgressDialog.xaml
+    /// </summary>
+    public partial class ProgressDialog : Window
+    {
+        /// <summary>
+        /// passthrough to the progress bar
+        /// </summary>
+        public double Value
+        {
+            get => ProgressBar.Value;
+            set => ProgressBar.Value = value;
+        }
+
+        internal ProgressDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
