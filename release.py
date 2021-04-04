09d2dee6b114f8605d427f0348b4c409d0462eec
Mike Meyer <mike@meyermike.com>
2021-04-04 12:45:47 -0400

    release

--- release.py
+++ release.py
@@ -19,11 +19,14 @@ def find_current_version():
     raise Exception("Failed to find current version")
 
 old_version = find_current_version()
+print(f'previous version: {old_version}')
 split_ver = old_version.split('.')
 split_ver[2] = str(int(split_ver[2]) + 1)
 version = '.'.join(split_ver)
+print(f'new version: {version}')
 
 for path in assembly_info_files:
+    print(f'updating file {path}')
     with fileinput.input(path, inplace=True) as f:
         for line in f:
             m = assembly_re.match(line)
@@ -34,6 +37,7 @@ for path in assembly_info_files:
 
 # wix installer
 with fileinput.input(wxs_path, inplace=True) as f:
+    print(f'updating file {path}')
     for line in f:
         m = wsx_re.match(line)
         if m:
@@ -44,6 +48,7 @@ with fileinput.input(wxs_path, inplace=True) as f:
 #
 # BUILD
 #
+print(f'building installer solution')
 import subprocess
 result = subprocess.run([r'C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe', 'Installer/Installer.wixproj', '/p:Configuration=Release'])
 if 0 != result.returncode:
@@ -58,6 +63,7 @@ except ModuleNotFoundError:
     import subprocess
     subprocess.run("python -m pip install --upgrade pip")
     subprocess.run("python -m pip install --upgrade paramiko")
+    import paramiko
 import json
 login_info = json.load(open('ftpinfo.json', "r"))
 host = login_info["host"]
@@ -74,6 +80,7 @@ def create_sftp():
     return sftp
     
 with create_sftp() as sftp:
+    print(f'uploading to server')
     sftp.put('Installer/bin/Release/BloodstarClockticaInstaller.msi', 'BloodstarClockticaInstaller.msi')
 
 
