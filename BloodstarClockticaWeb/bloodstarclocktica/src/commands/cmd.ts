e5efc48220012b3d660afc0f75c9b8c20e8312c3
Mike Meyer <mike@meyermike.com>
2021-05-12 17:03:28 -0400

    retry once on command failure

--- BloodstarClockticaWeb/bloodstarclocktica/src/commands/cmd.ts
+++ BloodstarClockticaWeb/bloodstarclocktica/src/commands/cmd.ts
@@ -4,30 +4,34 @@
  */
 
 import { spinner } from "../dlg/spinner-dlg";
-import {show as showMessage, showError} from "../dlg/blood-message-dlg";
+
+const TIMEOUT = 15*1000;
+const MAXRETRIES = 1;
 
 /**
  * send a command to the server, await response
  * Brings up the loading spinner during the operation
  */
- export default async function cmd(username:string, password:string, cmdName:string, spinnerMessage:string, body?:BodyInit|null|undefined):Promise<unknown> {
-    return await spinner('command', spinnerMessage, _cmd(username, password, cmdName, body));
+ export default async function cmd(username:string, password:string, cmdName:string, spinnerMessage:string, body?:BodyInit):Promise<unknown> {
+    return await spinner('command', spinnerMessage, _cmd(username, password, cmdName, body, TIMEOUT, MAXRETRIES));
 }
 
-/**
- * send a command to the server, await response 
- */
-async function _cmd(username:string, password:string, cmdName:string, body?:BodyInit|null):Promise<unknown> {
-    let response:Response;
+/** wrap fetch to timeout and automatically retry */
+async function fetchWithTimeoutAndRetry(username:string, password:string, cmdName:string, body:BodyInit|undefined, timeout:number, maxRetries:number):Promise<Response> {
     const base64 = btoa(`${username}:${password}`);
     const controller = new AbortController();
-    const timeoutId = setTimeout(()=>{
-        controller.abort();
-        // intentional floating promise - TODO: something to prevent getting many of these at once
-        void showMessage('Network Error', `Request timed out for command "${cmdName}"`);
-    }, 15*1000);
-    try {
-        response = await fetch(`https://www.bloodstar.xyz/cmd/${cmdName}.php`, {
+    return await new Promise((resolve,reject)=>{
+        const timeoutId = setTimeout(()=>{
+            controller.abort();
+            if (maxRetries > 0) {
+                console.log(`Request timed out for command "${cmdName}. ${maxRetries} retries remaining"`);
+                resolve(fetchWithTimeoutAndRetry(username, password, cmdName, body, timeout, maxRetries - 1));
+            } else {
+                reject(`Request timed out for command "${cmdName}"`);
+            }
+        }, timeout);
+
+        fetch(`https://www.bloodstar.xyz/cmd/${cmdName}.php`, {
                 method: 'POST',
                 headers:{
                     'Authorization': `Basic ${base64}`
@@ -36,30 +40,34 @@ async function _cmd(username:string, password:string, cmdName:string, body?:Body
                 credentials: 'include',
                 signal: controller.signal,
                 body
-            });
-        
-        if (!response.ok) {
-            const error = `${response.status}: (${response.type}) ${response.statusText}`;
-            // intentional floating promise - TODO: something to prevent getting many of these at once
-            void showError('Network Error', `Error encountered during command ${cmdName}`, error);
-            console.error(error);
-            return null;
-        }
-    } catch (error) {
-        // intentional floating promise - TODO: something to prevent getting many of these at once
-        void showError('Network Error', `Error encountered during command ${cmdName}`, error);
-        return null;
-    } finally {
-        clearTimeout(timeoutId);
+            })
+            .finally(()=>clearTimeout(timeoutId))
+            .then(resolve, reject);
+    });
+}
+
+/**
+ * send a command to the server, await response 
+ */
+async function _cmd(username:string, password:string, cmdName:string, body:BodyInit|undefined, timeout:number, maxRetries:number):Promise<unknown> {
+    const response = await fetchWithTimeoutAndRetry(
+        username,
+        password,
+        cmdName,
+        body,
+        timeout,
+        maxRetries
+    );
+    
+    if (!response.ok) {
+        throw new Error(`${response.status}: (${response.type}) ${response.statusText}`);
     }
 
+    const responseText = await response.text();
+    
     try {
-        const responseText = await response.text();
         return JSON.parse(responseText);
     } catch (error) {
-        // intentional floating promise - TODO: something to prevent getting many of these at once
-        void showError('Error', `Error parsing server response JSON for command ${cmdName}`, error);
-        console.error(error);
+        throw new Error(`Error parsing server response JSON.`);
     }
-    return {};
 }
