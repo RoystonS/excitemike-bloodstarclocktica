b1bfac95fc64535496235e137bd1c26dc8907b6b
Mike Meyer <mike@meyermike.com>
2021-08-23 16:53:13 -0400

    clear recent file when creating a new one

--- bloodstar.xyz/src/commands/new.ts
+++ bloodstar.xyz/src/commands/new.ts
@@ -4,10 +4,13 @@
  */
 import {Edition} from '../model/edition';
 import * as SdcDlg from '../dlg/blood-save-discard-cancel';
+import { clearRecentFile } from '../recent-file';
 
 /// prompt for save if needed, then reset to new custom edition
 export async function newEdition(edition:Edition):Promise<boolean> {
     if (await SdcDlg.savePromptIfDirty(edition)) {
+        const oldName = edition.saveName.get();
+        clearRecentFile(oldName);
         await edition.reset();
         return true;
     }
