8f76bef12347467c3744bc6809065c03cb969990
Mike Meyer <mike@meyermike.com>
2021-08-31 13:25:39 -0400

    Can now list shared files

--- bloodstar.xyz/dist/api/list.php
+++ bloodstar.xyz/dist/api/list.php
@@ -18,15 +18,43 @@
         exit();
     }
 
-    $list = array();
+    $yourFiles = array();
     foreach ($items as $key => $value) {
         if ($value == '.') {continue;}
         if ($value == '..') {continue;}
         $filepath = join_paths($userSaveDir, $value);
         if (is_dir($filepath)) {
-            array_push($list, $value);
+            array_push($yourFiles, $value);
         }
     }
 
-    echo json_encode(array('files' => $list));
+    // old client expects only a 'files' field
+    if (!array_key_exists('includeShared', $request)) {
+        echo json_encode(array('files' => $yourFiles));
+        exit();
+    }
+    
+    $sharedFiles = getSharedFiles($username);
+    echo json_encode(array('files' => $yourFiles, 'shared' => $sharedFiles));
+
+    function getSharedFiles($username) {
+        $mysqli = makeMysqli();
+        $escapedUser = $mysqli->real_escape_string($username);
+        $result = $mysqli->query("SELECT `share`.`owner`, `share`.`edition` FROM `share` WHERE `share`.`user` = '$escapedUser' OR `share`.`user` = 'EVERYONE';");
+        if (false===$result){
+            echo json_encode(array("error" => 'sql error'));
+            exit();
+        }
+        $sharedFiles = array();
+        $results = $result->fetch_all();
+        foreach ($results as $row) {
+            list($owner, $edition) = $row;
+            if (!array_key_exists($owner, $sharedFiles)) {
+                $sharedFiles[$owner] = array();
+            }
+            array_push($sharedFiles[$owner], $edition);
+        }
+
+        return $sharedFiles;
+    }
 ?>
