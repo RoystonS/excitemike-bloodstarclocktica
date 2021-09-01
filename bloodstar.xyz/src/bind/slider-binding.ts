488796efa2462d544270e1d72772ec925ab7d88a
Mike Meyer <mike@meyermike.com>
2021-09-01 09:32:08 -0400

    range slider tweaks

--- bloodstar.xyz/src/bind/slider-binding.ts
+++ bloodstar.xyz/src/bind/slider-binding.ts
@@ -2,11 +2,11 @@ import {BaseBinding, Property} from './base-binding';
 
 async function syncFromElementToProperty(element:HTMLInputElement, valueLabel:HTMLElement|null, property:Property<number>):Promise<void> {
     await property.set(parseFloat(element.value));
-    if (valueLabel) { valueLabel.innerText = element.value; }
+    if (valueLabel) { valueLabel.innerText = Number(element.value).toFixed(2); }
 }
 function syncFromPropertyToElement(element:HTMLInputElement, valueLabel:HTMLElement|null, property:Property<number>):void {
     element.value=String(property.get());
-    if (valueLabel) { valueLabel.innerText = element.value; }
+    if (valueLabel) { valueLabel.innerText = Number(element.value).toFixed(2); }
 }
 
 export default class SliderBinding  extends BaseBinding<number> {
