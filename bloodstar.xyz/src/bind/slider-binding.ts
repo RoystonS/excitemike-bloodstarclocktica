5dbde27bdd417335d95940cf76a09b1699ad9b65
Mike Meyer <mike@meyermike.com>
2021-09-01 10:51:15 -0400

    range slider value labels
    some benefit from decimal places, some don't

--- bloodstar.xyz/src/bind/slider-binding.ts
+++ bloodstar.xyz/src/bind/slider-binding.ts
@@ -1,12 +1,23 @@
 import {BaseBinding, Property} from './base-binding';
 
+function updateValueLabel(element:HTMLInputElement, valueLabel:HTMLElement|null) {
+    if (valueLabel) {
+        const places = valueLabel.dataset.places;
+        if (places === undefined) {
+            valueLabel.innerText = element.value;
+        } else {
+            valueLabel.innerText = Number(element.value).toFixed(Number(places));
+        }
+    }
+}
+
 async function syncFromElementToProperty(element:HTMLInputElement, valueLabel:HTMLElement|null, property:Property<number>):Promise<void> {
     await property.set(parseFloat(element.value));
-    if (valueLabel) { valueLabel.innerText = Number(element.value).toFixed(2); }
+    updateValueLabel(element, valueLabel);
 }
 function syncFromPropertyToElement(element:HTMLInputElement, valueLabel:HTMLElement|null, property:Property<number>):void {
     element.value=String(property.get());
-    if (valueLabel) { valueLabel.innerText = Number(element.value).toFixed(2); }
+    updateValueLabel(element, valueLabel);
 }
 
 export default class SliderBinding  extends BaseBinding<number> {
