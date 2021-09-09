37f04bb97d22c3b52d746c2a859299c92c110607
Mike Meyer <mike@meyermike.com>
2021-09-09 17:39:41 -0400

    publish now uses genericCmd

--- bloodstar.xyz/src/commands/publish.ts
+++ bloodstar.xyz/src/commands/publish.ts
@@ -3,44 +3,34 @@
  * @module Publish
  */
 import {Edition} from '../model/edition';
-import { showError } from '../dlg/blood-message-dlg';
 import { AriaDialog } from '../dlg/aria-dlg';
-import signIn, { signedInCmd } from '../sign-in';
+import genericCmd from './generic-cmd';
 
-type PublishData = {
+type PublishRequest = {
     saveName:string;
     token:string;
 };
-type PublishReturn = {error?:string; script:string; almanac:string};
+type PublishResponse = {success:true; script:string; almanac:string};
 
 /**
  * publish the edition
  */
 export default async function publish(edition:Edition):Promise<boolean> {
-    const sessionInfo = await signIn({
-        title:'Sign In to Publish',
-        message:'You must be signed in to publish.'
-    });
-    if (!sessionInfo) {return false;}
     const saveName = edition.saveName.get();
-    const saveData:PublishData = {
-        token:sessionInfo.token,
-        saveName: saveName
-    };
+    const result = await genericCmd<PublishRequest, PublishResponse>({
+        command:'publish',
+        errorMessage:'Error encountered during publish',
+        request:sessionInfo=>({
+            token:sessionInfo?.token??'',
+            saveName: saveName
+        }),
+        signIn:{title:'Sign In to Publish', message:'You must be signed in to publish.'},
+        spinnerMessage:`Publishing ${edition.saveName.get()}`
+    });
 
-    let response:PublishReturn;
-    try {
-        response = await signedInCmd('publish', `Publishing ${edition.saveName.get()}`, saveData);
-    } catch (error: unknown) {
-        await showError('Error', 'Error encountered during publish', error);
-        return false;
-    }
-    const {error} = response;
-    if (error) {
-        await showError('Error', 'Error encountered during publish', error);
-        return false;
-    }
-    const {script, almanac} = response;
+    if ('error' in result) {return false;}
+    if ('cancel' in result) {return false;}
+    const {script, almanac} = result.data;
     const cacheTrickingScriptLink = `${script}?${(Date.now() % (31*24*60*60*1000)).toString(16)}`;
 
     const copyLink = async (evt: Event, link:string)=>{
