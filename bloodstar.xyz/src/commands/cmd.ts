e2277cd8457b961510797045de086c06fe92dbd2
Mike Meyer <mike@meyermike.com>
2021-09-13 09:23:32 -0400

    cmd now abortable

--- bloodstar.xyz/src/commands/cmd.ts
+++ bloodstar.xyz/src/commands/cmd.ts
@@ -10,62 +10,91 @@ const MAXRETRIES = 1;
 
 export type UserPass = {username:string; password:string};
 
+export class TimeoutError extends Error {}
+
+/** track abort controllers that we know were used for a timeout */
+const timeoutControllers = new WeakSet<AbortController>();
+
 /**
  * send a command to the server, await response
  * Brings up the loading spinner during the operation
  */
-export default async function cmd<ResultType = unknown>(cmdName:string, spinnerMessage:string, body?:BodyInit):Promise<ResultType> {
-    return spinner<ResultType>(spinnerMessage, _cmd<ResultType>(cmdName, body, TIMEOUT, MAXRETRIES));
+export default async function cmd<ResultType = unknown>(cmdName:string, spinnerMessage:string, body?:BodyInit, controller?:AbortController):Promise<ResultType> {
+    return spinner(spinnerMessage, _cmd(cmdName, body, TIMEOUT, MAXRETRIES, controller));
 }
 
 /**
- * wrap fetch with a timeout
+ * Wrap fetch with a timeout.
+ * If provided, the given controller will be aborted if there is a network error.
  */
-async function fetchWithTimeout(cmdName:string, body:BodyInit|undefined, timeout:number):Promise<Response> {
-    const controller = new AbortController();
-    let timedOut = false;
+async function fetchWithTimeout(cmdName:string, body:BodyInit|undefined, timeout:number, controller?:AbortController):Promise<Response> {
+    const controllerSafe = controller ?? new AbortController();
     const timeoutId = setTimeout(()=>{
-        timedOut = true;
-        controller.abort();
+        if (!controllerSafe.signal.aborted) {
+            timeoutControllers.add(controllerSafe);
+            controllerSafe.abort();
+        }
     }, timeout);
 
     return fetch(`https://www.bloodstar.xyz/api/${cmdName}.php`, {
         method: 'POST',
         mode: 'cors',
-        signal: controller.signal,
+        signal: controllerSafe.signal,
         body
-    }).catch(()=>{
-        throw new Error(timedOut?`Command "${cmdName}"" timed out`:`Network error during command "${cmdName}"`);
+    }).catch((e:unknown)=>{
+        if (!controllerSafe.signal.aborted) {
+            controllerSafe.abort();
+        }
+        if ((e instanceof DOMException) && (e.name === 'AbortError')) {
+            if (timeoutControllers.has(controllerSafe)) {
+                throw new TimeoutError(`Command "${cmdName}" timed out`);
+            }
+        }
+        throw e;
     }).finally(()=>{
         clearTimeout(timeoutId);
     });
 }
 
 /**
- * wrap fetch to timeout and automatically retry
+ * Wrap fetch to timeout and automatically retry.
+ * If provided, the given controller is used only on the last retry.
  */
-async function fetchWithTimeoutAndRetry(cmdName:string, body:BodyInit|undefined, timeout:number, maxRetries:number):Promise<Response> {
+async function fetchWithTimeoutAndRetry(
+    cmdName:string,
+    body:BodyInit|undefined,
+    timeout:number,
+    maxRetries:number,
+    controller?:AbortController
+):Promise<Response> {
     let retriesLeft = maxRetries;
     while (retriesLeft > 0) {
         try {
             return await fetchWithTimeout(cmdName, body, timeout);
-        } catch (e: unknown) {
-            if (maxRetries <= 0) {throw e;}
-            retriesLeft--;
+        } catch (e:unknown) {
+            // ignore and try again
         }
+        retriesLeft--;
     }
-    return fetchWithTimeout(cmdName, body, timeout);
+    return fetchWithTimeout(cmdName, body, timeout, controller);
 }
 
 /**
  * send a command to the server, await response
  */
-async function _cmd<ResultType = unknown>(cmdName:string, body:BodyInit|undefined, timeout:number, maxRetries:number):Promise<ResultType> {
+async function _cmd<ResultType = unknown>(
+    cmdName:string,
+    body:BodyInit|undefined,
+    timeout:number,
+    maxRetries:number,
+    controller?:AbortController
+):Promise<ResultType> {
     const response = await fetchWithTimeoutAndRetry(
         cmdName,
         body,
         timeout,
-        maxRetries
+        maxRetries,
+        controller
     );
 
     if (!response.ok) {
