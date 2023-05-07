2289c6c3857736bb2adfd0ea01a81fd8280c93b6
Mike Meyer <mike@meyermike.com>
2023-05-06 23:34:15 -0400

    Adding donate button to almanac

--- bloodstar.xyz/dist/api/almanac.php
+++ bloodstar.xyz/dist/api/almanac.php
@@ -24,6 +24,14 @@
 .makeAlmanacItems($saveData, $username, $saveName)
 .makeNightOrder($saveData, $username, $saveName)
 .'<li class="generated-by">this almanac generated using <a href="https://www.bloodstar.xyz">Bloodstar Clocktica</a></li>'
+.'<li class="donate"><span>Enjoying Bloodstar? Buy me a coffee!</span><form action="https://www.paypal.com/donate" method="post" target="_top">
+    <input type="hidden" name="business" value="XETJMX9JKLGKQ" />
+    <input type="hidden" name="no_recurring" value="0" />
+    <input type="hidden" name="item_name" value="I shared Bloodstar for BotC fans to use for free, but any help with the costs for storing all those images is appreciated!" />
+    <input type="hidden" name="currency_code" value="USD" />
+    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+    <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+    </form></li>'
 .'</ol></div></body></html>';
     }
 
